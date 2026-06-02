fe6bc1adf66eb9ee89b23e525ee648794e515781
Sparsh Agrawal <sparshagrawal2806@gmail.com>
2026-06-02 12:20:26 +0530

    Added Contains Duplicates

---
+++ Week_1/Day_2/Contains_Duplicates.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> freq;
+
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        
+          if(freq[nums[i]]>1){
+            return true;
+           }
+        }
+        return false;
+    }
+};
