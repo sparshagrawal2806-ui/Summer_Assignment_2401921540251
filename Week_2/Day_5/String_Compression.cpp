4d3ffd1ecd70a43e19a99b579537505b817f3f42
Sparsh Agrawal <sparshagrawal2806@gmail.com>
2026-06-12 16:22:39 +0530

    Added String Compression

--- Week_2/Day_5/String_Compression.cpp
+++ Week_2/Day_5/String_Compression.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         int n = chars.size();
-        int index = 0;  // write pointer
-        int i = 0;      // read pointer
+        int index = 0;
+        int i = 0;
 
         while (i < n) {
             char curr = chars[i];
