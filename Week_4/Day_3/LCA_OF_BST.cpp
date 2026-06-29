2df1ef7d8a628c65ad72d23105d40d18f9049a84
Sparsh Agrawal <sparshagrawal2806@gmail.com>
2026-06-29 13:42:04 +0530

    Added LCA Of Binary Search Tree

---
+++ Week_4/Day_3/LCA_OF_BST.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root != nullptr){
+            
+            if(root->val > p->val && root->val
+             > q->val )
+                root = root->left;
+            
+            else if (root->val < p->val && root->val < q->val)
+                root=root->right;
+            
+            else 
+                break;
+            
+        }
+        
+        return root;
+
+    }
+};
